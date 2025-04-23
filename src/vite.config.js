d68cb37a37853a89a147cb3906addd3b21415f23
李俊彦 <147243735+liQingning@users.noreply.github.com>
2025-04-23 18:56:35 +0800

    调整路径

--- src/vite.config.js
+++ src/vite.config.js
@@ -4,6 +4,16 @@ import path from 'path'
 
 export default defineConfig({
     base: '/SU7-webgi/',
+    build: {
+        assetsDir: 'assets',
+        rollupOptions: {
+            output: {
+                // 启用哈希命名，避免缓存问题
+                assetFileNames: 'assets/[name]-[hash][extname]',
+                entryFileNames: 'assets/[name]-[hash].js'
+            }
+        }
+    },
     plugins: [
         viteStaticCopy({
             targets: [
